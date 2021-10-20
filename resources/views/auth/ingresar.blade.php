2f009c757c1349f6e9dc50c4c0d38762a87a25cd
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-10-20 19:05:53 -0300

    Login captcha moved

--- resources/views/auth/ingresar.blade.php
+++ resources/views/auth/ingresar.blade.php
@@ -58,15 +58,15 @@
                             </li>
                         </ul>
                     </div>
-                    <div class="flex items-center justify-center lg:justify-center py-4">
-                        <button class="form-submit ingresar flex justify-center submit btn btn-dos p-2 px-8 my-4" type="submit">Ingresar</button>
-                    </div>
                     {!! app('captcha')->display() !!}
                     @if ($errors->has('g-recaptcha-response'))
                         <span class="support support-box support-g-recaptcha-response error pl-1 font-bold">{{ $errors->first('g-recaptcha-response') }}</span>
                     @else
                         <span class="support support-box support-g-recaptcha-response error pl-1 font-bold"></span>
                     @endif
+                    <div class="flex items-center justify-center lg:justify-center py-4">
+                        <button class="form-submit ingresar flex justify-center submit btn btn-dos p-2 px-8 my-4" type="submit">Ingresar</button>
+                    </div>
                     <div class="loguear text-center pb-8">
                         <p>
                             <span>¿No tenés cuenta?</span>
