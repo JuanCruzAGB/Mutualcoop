4f5a19eaaf07222ceac379f395967aeb756b4193
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-10-20 19:15:51 -0300

    Login captcha moved

--- resources/views/auth/ingresar.blade.php
+++ resources/views/auth/ingresar.blade.php
@@ -50,6 +50,13 @@
                             <span class="support support-box support-clave error w-full"></span>
                         @endif
                     </div>
+                    {{-- {!! app('captcha')->display() !!} --}}
+                    {!! NoCaptcha::display() !!}
+                    @if ($errors->has('g-recaptcha-response'))
+                        <span class="support support-box support-g-recaptcha-response error pl-1 font-bold">{{ $errors->first('g-recaptcha-response') }}</span>
+                    @else
+                        <span class="support support-box support-g-recaptcha-response error pl-1 font-bold"></span>
+                    @endif
                     <div class="checkbox-obras w-8/12 mx-auto xl:w-8/12">
                         <ul class="obras my-8">
                             <li class="checkbox mx-auto w-8/12 md:w-7/12 lg:w-10/12">
@@ -58,13 +65,6 @@
                             </li>
                         </ul>
                     </div>
-                    {{-- {!! app('captcha')->display() !!} --}}
-                    {!! NoCaptcha::display() !!}
-                    @if ($errors->has('g-recaptcha-response'))
-                        <span class="support support-box support-g-recaptcha-response error pl-1 font-bold">{{ $errors->first('g-recaptcha-response') }}</span>
-                    @else
-                        <span class="support support-box support-g-recaptcha-response error pl-1 font-bold"></span>
-                    @endif
                     <div class="flex items-center justify-center lg:justify-center py-4">
                         <button class="form-submit ingresar flex justify-center submit btn btn-dos p-2 px-8 my-4" type="submit">Ingresar</button>
                     </div>
